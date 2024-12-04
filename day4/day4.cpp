144e7a1dba1b08742a4edfe1f2fdb62bfb839656
Ong Zhi Li <122247557+ongzhili@users.noreply.github.com>
2024-12-04 05:26:12 +0000

    Setup day 4 files

---
+++ day4/day4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <regex>
+#include <string>
+using namespace std;
+
+void part1() {
+
+}
+
+void part2() {
+
+
+}
+
+int main() {
+    part1();
+    cout << "\n";
+    part2();
+}
