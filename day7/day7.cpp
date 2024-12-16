4238f813c696e75959dadf5daec32cb9ca713c30
Ong Zhi Li <122247557+ongzhili@users.noreply.github.com>
2024-12-16 03:11:42 +0000

    Init day7

---
+++ day7/day7.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+void part1() {
+
+}
+
+void part2() {
+
+}
+
+int main() {
+    part1();
+    cout << "\n";
+    part2();
+}
